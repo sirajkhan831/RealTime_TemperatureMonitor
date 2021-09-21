5473595fba7ac1bd394604fda2fbae618405f52f
Siraj <sirajkhanc2305@gmail.com>
2021-09-21 16:01:13 +0530

    Graphical Data added

--- src/main/java/Weather.java
+++ src/main/java/Weather.java
@@ -3,6 +3,7 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
+import java.awt.*;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -15,8 +16,9 @@ import java.util.Scanner;
 
 public class Weather {
     public static void main(String[] args) throws IOException, InterruptedException {
+        System.out.println("Welcome to the real time temperature monitoring tool");
         Gson gson = new Gson();
-        System.out.print("Enter your city to get it's temperature data : ");
+        System.out.print("Enter your city's name to get it's temperature data : ");
         String city = new Scanner(System.in).nextLine();
         var urlGeo = "https://api.opencagedata.com/geocode/v1/json?q="+city+"&key=a238b3abbc004c6198a4b83580fd7a4f";
         var requestGeo = HttpRequest.newBuilder().GET().uri(URI.create(urlGeo)).build();
@@ -27,7 +29,7 @@ public class Weather {
         JsonElement lng = jsonObject.get("results").getAsJsonArray().get(0).getAsJsonObject().get("bounds").getAsJsonObject().get("northeast").getAsJsonObject().get("lng");
         String latString = lat.getAsString().substring(0, 4);
         String lngString = lng.getAsString().substring(0, 4);
-        System.out.println("Searching for city "+city+" ("+latString + "N " + lngString+"E) ");
+        System.out.println("Searching for city "+city+" ("+latString + "N " + lngString+"E) (may take a few seconds depending upon server load)");
         var url = "https://www.7timer.info/bin/astro.php?lon="+lngString+"&lat="+latString+"&ac=0&unit=metric&output=json";
         var request = HttpRequest.newBuilder().GET().uri(URI.create(url)).build();
         var client = HttpClient.newBuilder().build();
@@ -51,5 +53,12 @@ public class Weather {
             case 24 -> index = 7;
         }
         System.out.println("The temperature of "+city+" at " + LocalTime.now().format(DateTimeFormatter.ISO_TIME).substring(0, 5) + " is -> " + object.get("dataseries").getAsJsonArray().get(index).getAsJsonObject().get("temp2m") + " degree celsius.");
+        System.out.println("To view the graphical data press 1 To exit press 2");
+        if (new Scanner(System.in).nextInt() == 1)
+        {
+
+            Desktop.getDesktop().browse(URI.create("http://www.7timer.info/bin/astro.php?lon="+lngString+"&lat="+latString+"&ac=0&lang=en&unit=metric&output=internal&tzshift=0"));
+        }
+        else System.exit(1);
     }
 }
